6dbe24e4a8180fdf300af777395b3080e4d71da4
Richard Garcia <richardgarciaph@gmail.com>
2022-01-05 21:37:37 +1300

    add comment

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,6 +4,7 @@ namespace HelloWorld
 {
     class Program
     {
+        //Entry point of our program 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
